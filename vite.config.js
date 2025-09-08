a786394b07b2bcdf293af48b3630449baf5cebc2
Jon Henry <jonnypit@outlook.com>
2025-09-07 22:13:48 -0400

    test change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/jhenry-portfolio/',   // IMPORTANT: must match your repo name
+  base: '/jonnypit.github.io/',   // IMPORTANT: must match your repo name
   build: {
     outDir: 'docs',
     emptyOutDir: true,
