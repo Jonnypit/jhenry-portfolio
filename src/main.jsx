6f020e81850530d8309f233a4bdd032823ee12f4
Jon Henry <jonnypit@outlook.com>
2025-09-07 22:03:23 -0400

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,13 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-// import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <BrowserRouter basename={import.meta.env.BASE_URL}>
-//       <App />
-//     </BrowserRouter>
-//   </StrictMode>
-// );
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import mdx from '@mdx-js/rollup'
-
-export default defineConfig({
-  plugins: [
-    { enforce: 'pre', ...mdx() },
-    react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
-  ],
-})
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
