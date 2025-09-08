a24efdb374fb84d9532291455f8484d6b79a452f
Jon Henry <jonnypit@outlook.com>
2025-09-07 21:34:02 -0400

    config change

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // For GitHub Pages at https://jonnypit.github.io/jhenry-portfolio/
-  // If/when you switch to a custom domain (symbioticcity.tech), set base to "/".
-  base: "/jhenry-portfolio/", // Remember to switch to "/" if using a custom domain
   plugins: [react()],
-  build: { outDir: "docs" }  // Pages: Settings → Source: main /docs
-<<<<<<< HEAD
-});
-=======
-});
->>>>>>> 1e1631f (Initial commit with portfolio site files)
+})
